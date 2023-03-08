b5f45dcd0254136f68bab65455f2209a56f0be40
Quentin <109299545+ClueXIII@users.noreply.github.com>
2023-03-08 13:29:31 +0100

    add send_w_bluetooh + collect_data.js

---
+++ src/arduino_prog/send_w_bluetooh.cpp
@@ -0,0 +1,25 @@
+#include <SoftwareSerial.h>
+
+// Pin utilisée pour la communication Bluetooth
+#define RX_PIN 2
+#define TX_PIN 3
+
+// Initialise le port série Bluetooth
+SoftwareSerial bluetooth(RX_PIN, TX_PIN);
+
+// variable contenant le nombre de personnes
+int personnes = 42;
+
+void setup() {
+    // Initialise la communication série à 9600 bauds
+    Serial.begin(9600);
+    // Initialise la communication Bluetooth à 9600 bauds
+    bluetooth.begin(9600);
+}
+
+void loop() {
+    // Envoie le nombre de personnes en Bluetooth
+    bluetooth.print(variable);
+    delay(1000);
+}
+
---
+++ src/js/collect_data.js
@@ -0,0 +1,23 @@
+const btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
+
+// Adresse MAC de l'Arduino Bluetooth
+const address = '00:00:00:00:00:00';
+
+// UUID du service Bluetooth
+const uuid = '00001101-0000-1000-8000-00805F9B34FB';
+
+// Connecte à l'Arduino Bluetooth
+btSerial.connect(address, 1, function() {
+    console.log('Connected to Arduino Bluetooth.');
+
+    // Récupère la valeur envoyée par l'Arduino
+    btSerial.on('data', function(buffer) {
+        const data = buffer.readUInt8(0);
+        console.log('Received data: ' + data);
+    });
+}, function() {
+    console.log('Failed to connect to Arduino Bluetooth.');
+});
+
+// Ferme la connexion Bluetooth
+btSerial.close();
